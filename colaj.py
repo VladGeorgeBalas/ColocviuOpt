620bbceb0cb35270730e9b4426d0f8269cbd2279
VladGeorgeBalas <157060252+VladGeorgeBalas@users.noreply.github.com>
2025-05-19 19:50:17 +0300

    gata

--- colaj.py
+++ colaj.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 
-def colaj(original, noise, grad_pro, grad_bar, cvx):
-    colaj = cv2.hconcat([original.astype("uint8"), noise.astype("uint8"), grad_pro.astype("uint8"), grad_bar.astype("uint8"), cvx.astype("uint8")])
+def colaj(original, noise, org_grad_pro, org_grad_bar, org_grad_cvx, no_grad_pro, no_grad_bar, no_grad_cvx):
+    colaj_sus = cv2.hconcat([original.astype("uint8"), org_grad_pro.astype("uint8"), org_grad_bar.astype("uint8"), org_grad_cvx.astype("uint8")])
+    colaj_jos= cv2.hconcat([noise.astype("uint8"), no_grad_pro.astype("uint8"), no_grad_bar.astype("uint8"), no_grad_cvx.astype("uint8")])
 
-    return colaj
+    return cv2.vconcat([colaj_sus, colaj_jos])
 
 def add_gaussian_noise(image, mean=0, var=100):
     """Add Gaussian noise to a grayscale image."""
